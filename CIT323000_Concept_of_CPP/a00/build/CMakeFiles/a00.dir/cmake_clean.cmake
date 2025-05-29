3f2950fc3fec8b1c2ef371d5da98275434a8e968
kai <ge62meq@mytum.de>
2025-05-29 12:49:14 +0200

    {{date}}

---
+++ CIT323000_Concept_of_CPP/a00/build/CMakeFiles/a00.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a00.dir/src/main.cpp.o"
+  "CMakeFiles/a00.dir/src/main.cpp.o.d"
+  "a00"
+  "a00.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a00.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
