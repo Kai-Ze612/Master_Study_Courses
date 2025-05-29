314c4e1eec3e63a0b3a2a7e7f30f00d17d8b3ec9
kai <ge62meq@mytum.de>
2025-05-29 18:19:02 +0200

    29/05/2025 18:18

---
+++ CPP_Practice/OOP_1/build/CMakeFiles/OOP_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OOP_1.dir/src/main.cpp.o"
+  "CMakeFiles/OOP_1.dir/src/main.cpp.o.d"
+  "OOP_1"
+  "OOP_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OOP_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- CPP_Practice/OOP_1/src/main.cpp
+++ CPP_Practice/OOP_1/src/main.cpp
@@ -1,13 +1,79 @@
 #include <iostream>
 #include <string>
 
-class Robot {
-    // private: used to define parameters that are not accessible outside the class
-private:
-    std::string type;
-    double batteryLevel;
+class Robot{
+    // Private define the variables that are not accessible outside the class
+    private:
+        std::string robot_name;
+        double battery_level;
 
-    // public: used to define parameters that are accessible outside the class
-public:
+    // Public define the methods that are accessible outside the class
+    public:
+        // Constructor - called when object is created
+        Robot(std::string& name, double default_battery = 100.0):
+            robot_name(name), battery_level(default_battery) {
+                std::cout << "Robot " << robot_name << " with battery level " << battery_level << " created successfully." << std::endl;
+            }
+        
+        // Destructor - called when object is destroyed
+        ~Robot() {
+            std::cout << "Robot " << robot_name << " is being destroyed." << std::endl;
+        }
+        
+        // Member function to operate computation within the class
+        void movement() {
+            if (battery_level > 0){
+                std::cout << "Robot " << robot_name << " is moving." << std::endl;
+                battery_level -= 10; // Decrease battery level by 10
+            } else {
+                std::cout << "Robot " << robot_name << " has no battery left to move." << std::endl;
+            }
+        }
+
+        void fly(){
+            if (battery_level > 20){
+                std::cout << "Robot " << robot_name << " is flying." << std::endl;
+                battery_level -= 20; // Decrease battery level by 20
+            } else {
+                std::cout << "Robot " << robot_name << " does not have enough battery to fly." << std::endl;
+            }
+        }
+
+        void recharge(double amount) {
+            battery_level += amount;
+            std::cout << "Robot " << robot_name << " recharged. New battery level: " << battery_level << std::endl;
+        }
+
+        // Getter function - marked as const because it does not modify the object
+        double getBatteryLevel() const {
+            std::cout << "Getting battery level for robot " << robot_name << ": " << battery_level << std::endl;
+            return battery_level;
+        }
+
+};
+
+int main(){
+    // create an object
+
+    std::string name = "Optimus";
+    double battery_level = 10;
+
+    // Create a Robot object using the constructor
+    Robot Optimus(name, battery_level);
+    std::cout << name << "robot is being created with battery level: " << battery_level << std::endl;
     
+    // Call the movement method
+    Optimus.movement();
+    // Call the fly method
+    Optimus.fly();
+    // Call the recharge method
+    Optimus.recharge(30);
+
+    // To get the current battery level
+    double current_battery = Optimus.getBatteryLevel();
+
+    // The destructor will be called automatically when the object goes out of scope
+    // It means after the main function ends, the destructor will be called
+    // The object will be destroyed and the destructor will print a message
+    return 0;
 }
