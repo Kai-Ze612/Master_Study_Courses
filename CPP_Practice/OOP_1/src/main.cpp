4afeba027671edfc3e12b325192a73aa70e71fdb
kai <ge62meq@mytum.de>
2025-05-29 15:48:21 +0200

    29/05/2025

---
+++ CPP_Practice/OOP_1/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class Robot {
+    // private: used to define parameters that are not accessible outside the class
+private:
+    std::string type;
+    double batteryLevel;
+
+    // public: used to define parameters that are accessible outside the class
+public:
+    
+}
